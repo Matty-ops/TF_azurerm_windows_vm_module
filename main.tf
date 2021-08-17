5bff6335b1322c9ede2dfcb189ed6aca0c2f00b1
Matthieu MINET <m.minet@tf1.onmicrosoft.com>
2021-08-17 17:29:41 +0200

    format files

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_windows_virtual_machine" "az_vm_windows" {
-  name                            = var.vm_name
-  computer_name                   = var.computer_name == "" ? var.vm_name : var.computer_name
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  size                            = var.vm_size
-  admin_username                  = var.admin_username
-  admin_password                  = var.admin_password
-  network_interface_ids           = var.network_interface_ids
+  name                  = var.vm_name
+  computer_name         = var.computer_name == "" ? var.vm_name : var.computer_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  network_interface_ids = var.network_interface_ids
 
   os_disk {
     caching              = var.os_disk_caching
